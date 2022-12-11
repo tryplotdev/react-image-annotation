497c80a308b7c89ea2e3aca7aa96702f886e4813
Ethan Phan <phuocqphan@gmail.com>
2022-12-11 15:17:05 -0500

    test adding type for selector

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import Annotation from './components/Annotation'
-export { default as defaultProps } from './components/defaultProps'
+import Annotation from "./components/Annotation";
+export { default as defaultProps } from "./components/defaultProps";
+export { default as RectangleSelector } from "./hocs/RectangleSelector";
+export { default as PointSelector } from "./hocs/PointSelector";
+export { default as OvalSelector } from "./hocs/OvalSelector";
 
-export default Annotation
+export default Annotation;
